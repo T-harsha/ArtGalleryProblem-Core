4e276d0cc2f8b924f3a95e5a940964e572ab38dd
T-harsha <harsha.02k@gmail.com>
2020-11-01 23:25:57 -0500

    Adding canvas and basic lines

---
+++ main.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, bg="white", width=600, height=400)
+canvas.pack()
+
+coords = {"x":0,"y":0,"x2":0,"y2":0}
+# keep a reference to all lines by keeping them in a list 
+lines = []
+
+def click(e):
+    # define start point for line
+    coords["x"] = e.x
+    coords["y"] = e.y
+
+    # create a line on this point and store it in the list
+    lines.append(canvas.create_line(coords["x"],coords["y"],coords["x"],coords["y"]))
+
+def drag(e):
+    # update the coordinates from the event
+    coords["x2"] = e.x
+    coords["y2"] = e.y
+
+    # Change the coordinates of the last created line to the new coordinates
+    canvas.coords(lines[-1], coords["x"],coords["y"],coords["x2"],coords["y2"])
+
+canvas.bind("<ButtonPress-1>", click)
+canvas.bind("<B1-Motion>", drag) 
+
+root.mainloop()
